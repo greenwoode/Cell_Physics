e97c7c33b2c05288e194e86d99dd3e4937b11c95
Happy_Ta <will231897@gmail.com>
2022-04-12 09:14:42 -0400

    update my .gitignore + comment change in cell.h

--- ConsoleApplication1/cell.h
+++ ConsoleApplication1/cell.h
@@ -97,6 +97,8 @@ class cell {
 	 /// <summary>
 	 /// Return amount of force this cell excert on the otherCell
 	 /// Imagine ourCell hit the other cell
+	 /// Our Cell = the cell we are focus on
+	 /// Other Cell = the cell we are hitting
 	 /// </summary>
 	 /// <param name="otherCell">: The cell which our cell is colliding with</param>
 	 /// <returns>Does not return anything but add a force vector to our cell and otherCell</returns>
