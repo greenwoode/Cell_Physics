39ed9a7bc6d4ac4556ea6f4808459af25b9862bc
Happy_Ta <will231897@gmail.com>
2022-04-14 19:44:11 -0400

    Update grid.cpp

--- ConsoleApplication1/grid.cpp
+++ ConsoleApplication1/grid.cpp
@@ -17,27 +17,35 @@ double grid::getMass(cell * self, int id) {
 	switch (id) {
 		case 0:
 			// air
+			return 0.02897;
 			break;
 		case 1:
 			// sand
+			return 0.002918475;
 			break;
 		case 2:
 			// water
+			return 0.001801528;
 			break;
 		case 3:
 			// snow
+			return 0.000090076;
 			break;
 		case 4:
 			// steam
+			// Half of water according to Wiki
+			return 0.000900764;
 			break;
 		case 5:
 			// ice
+			return 0.001621376;
 			break;
 		case 6:
 			// molten sand
 			break;
 		case 10:
 			// solid wall
+			return 1.0;
 			break;
 
 
